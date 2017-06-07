c289ae12c3b4c33dc3e0cff46a13db80ea178073
Kathleen <kathleenmcclay@gmail.com>
2017-06-07 13:35:12 -0400

    Last Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'pictures#index'
+resources :pictures
+
 get 'pictures' => 'pictures#index'
-get 'pictures/:id/edit' => "pictures#edit"
- patch 'pictures/:id' => "pictures#update"
+get 'pictures/new' => 'pictures#new' # this is also a new line of code
+get 'pictures/:id/edit' => 'pictures#edit'
+
 
 post 'pictures' => 'pictures#create' # this is a new line of code
-get 'pictures/new' => 'pictures#new' # this is also a new line of code
 
-get 'pictures/:id' => 'pictures#show', as: 'picture
-delete 'pictures/:id' => 'pictures#destroy'
 
-  root 'pictures#index'
+get 'pictures/:id' => 'pictures#show', as: 'picture'
+
+
+
+  post 'pictures' => 'pictures#create'
+  patch 'pictures/:id' => 'pictures#update'
+  delete 'pictures/:id' => 'pictures#destroy'
 end
