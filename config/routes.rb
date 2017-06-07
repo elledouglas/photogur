47124855406829ea3ad44a69fad965e73af28dcb
Kathleen <kathleenmcclay@gmail.com>
2017-06-07 13:25:21 -0400

    Created new view for edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'pictures#index'
 get 'pictures' => 'pictures#index'
+get 'pictures/:id/edit' => "pictures#edit"
+ patch 'pictures/:id' => "pictures#update"
 
 post 'pictures' => 'pictures#create' # this is a new line of code
 get 'pictures/new' => 'pictures#new' # this is also a new line of code
