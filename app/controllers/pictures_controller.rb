0e82b30e95fd9cfe8d44bcf5ba4923c91cc3807a
Kathleen <kathleenmcclay@gmail.com>
2017-06-07 22:09:48 -0400

    piture_controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,4 +3,22 @@ class PicturesController < ApplicationController
     @pictures = Picture.all
   end
 
-end
+  def new
+     @picture = Picture.new
+   end
+
+   def create
+     @picture = Picture.new(picture_params)
+     if @picture.save
+       # if the picture gets saved, generate a get request to "/pictures" (the index)
+       redirect_to "/pictures"
+     else
+       # otherwise render new.html.erb
+       render :new
+     end
+   end
+   def picture_params
+     { title: params[:picture][:title], artist: params[:picture][:artist], url: params[:picture][:url] }
+   end
+
+ end
