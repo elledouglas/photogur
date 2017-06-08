57a7c9a31e311a6750cf225c90e8cdff3033437b
Kathleen <kathleenmcclay@gmail.com>
2017-06-07 21:57:15 -0400

    Lesson 2 additional commits

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'pictures#index'
-resources :pictures
-
-get 'pictures' => 'pictures#index'
-get 'pictures/new' => 'pictures#new' # this is also a new line of code
-get 'pictures/:id/edit' => 'pictures#edit'
+  get 'pictures' => 'pictures#index'
 
 
-post 'pictures' => 'pictures#create' # this is a new line of code
-
-
-get 'pictures/:id' => 'pictures#show', as: 'picture'
-
-
-
-  post 'pictures' => 'pictures#create'
-  patch 'pictures/:id' => 'pictures#update'
-  delete 'pictures/:id' => 'pictures#destroy'
 end
