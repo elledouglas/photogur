146ee402946b25249227cbc703d38211eb2e637c
Kathleen <kathleenmcclay@gmail.com>
2017-06-08 09:05:57 -0400

    photo library addition

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'pictures#index'
   get 'pictures' => 'pictures#index'
 
+  get 'pictures/:id/edit' => "pictures#edit"
+  patch 'pictures/:id' => "pictures#update"
 
 end
